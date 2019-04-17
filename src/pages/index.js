3f987a75694ff334f1dc1a49de6b23271072c41f
yh <yanghao@zwtyun.com>
2019-04-17 19:37:20 +0800

    indexpage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,11 @@
-// import styles from './index.css';
 
+import React from 'react';
+import { Redirect } from 'umi';
 
-export default function() {
+function IndexPage() {
   return (
-    <div >
-      <div  />
-      <ul >
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li>
-          <a href="https://umijs.org/guide/getting-started.html">
-            Getting Started
-          </a>
-        </li>
-      </ul>
-    </div>
+    <Redirect to="indexpage" />
   );
 }
+
+export default IndexPage;
