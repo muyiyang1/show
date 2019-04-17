a2f9415b2303b7521013211f145ae8af22f70f88
yh <yanghao@zwtyun.com>
2019-04-17 19:09:54 +0800

    show

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,48 +1,48 @@
 export default {
-  dashboard: {
-    url: '/dashboard',
-    name: '概览',
+  datamarket: {
+    url: '/datamarket',
+    name: '数据市场',
     // intlkey: 'dashboard',
     icon: '',
   },
-  notebook: {
-    url: '/notebook',
-    name: '研究空间',
+  appcenter: {
+    url: '/appcenter',
+    name: '应用中心',
     // intlkey: 'notebook',
     icon: '',
   },
-  factor: {
-    url: '/factor',
-    name: '因子管理',
-    // intlkey: 'factor',
-    icon: '',
+  // factor: {
+  //   url: '/factor',
+  //   name: '因子管理',
+  //   // intlkey: 'factor',
+  //   icon: '',
 
-  },
+  // },
 
-  strategy: {
-    url: '/strategy',
-    name: '策略管理',
-    // intlkey: 'strategic',
-    icon: '',
+  // strategy: {
+  //   url: '/strategy',
+  //   name: '策略管理',
+  //   // intlkey: 'strategic',
+  //   icon: '',
 
-  },
-  performance: {
-    url: '/performance',
-    name: '绩效分析',
-    // intlkey: 'performance',
-    icon: '',
-  },
-  mock: {
-    url: '/simulated',
-    name: '模拟交易',
-    // intlkey: 'mock',
-    icon: '',
-  },
-  doc: {
-    url: '/doc',
-    name: '用户文档',
-    // intlkey: 'doc',
-    icon: '',
-  },
+  // },
+  // performance: {
+  //   url: '/performance',
+  //   name: '绩效分析',
+  //   // intlkey: 'performance',
+  //   icon: '',
+  // },
+  // mock: {
+  //   url: '/simulated',
+  //   name: '模拟交易',
+  //   // intlkey: 'mock',
+  //   icon: '',
+  // },
+  // doc: {
+  //   url: '/doc',
+  //   name: '用户文档',
+  //   // intlkey: 'doc',
+  //   icon: '',
+  // },
 
 };
---
+++ src/pages/indexpage/index.js
@@ -0,0 +1,73 @@
+
+import React from 'react';
+import { Card, List, Avatar } from 'antd';
+import Meta from 'antd/lib/card/Meta';
+// import { styles } from 'ansi-colors';
+import styles from './index.less';
+
+const moduleList = [
+  {
+    icon: 'https://ws4.sinaimg.cn/large/006tNc79ly1g1wkozvgoxj305k05k3yj.jpg',
+    title: '热门个股',
+    desc: '这是一句话模块介绍这是一句话模块介绍这是一句话模块介',
+    link: '/datamarket',
+    disabled: false,
+    id: 0,
+  },
+  {
+    icon: 'https://ws4.sinaimg.cn/large/006tNc79ly1g1wkozvgoxj305k05k3yj.jpg',
+    title: '负面舆情',
+    desc: '这是一句话模块介绍这是一句话模块介绍这是一句话模块介',
+    link: '/',
+    disabled: false,
+    id: 1,
+  },
+  {
+    icon: 'https://ws4.sinaimg.cn/large/006tNc79ly1g1wkozvgoxj305k05k3yj.jpg',
+    title: '新兴概念',
+    desc: '这是一句话模块介绍这是一句话模块介绍这是一句话模块介',
+    link: '/',
+    disabled: false,
+    id: 2,
+  },
+  {
+    icon: 'https://ws4.sinaimg.cn/large/006tNc79ly1g1wkozvgoxj305k05k3yj.jpg',
+    title: '热度关注',
+    desc: '这是一句话模块介绍这是一句话模块介绍这是一句话模块介',
+    // eslint-disable-next-line no-script-url
+    link: 'javascript:void(0)',
+    disabled: false,
+    id: 3,
+  },
+
+];
+
+function IndexPage() {
+  return (
+    <div className={styles.container}>
+      <h2>概览</h2>
+      <List
+        grid={{ gutter: 24, xs: 3 }}
+        dataSource={moduleList}
+        renderItem={item => (
+          <List.Item key={item.id} className={item.disabled ? styles.disabled : styles.abled}>
+            <Card
+              hoverable
+              actions={[
+                <a href={item.link}>更多</a>,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src={item.icon} />}
+                title={item.title}
+                description={item.desc}
+              />
+            </Card>
+          </List.Item>
+        )}
+      />
+    </div>
+  );
+}
+
+export default IndexPage;
