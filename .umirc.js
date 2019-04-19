6616180b5c9e5a95f0e1da59384896a0ecaf781e
yh <yanghao@zwtyun.com>
2019-04-19 12:20:50 +0800

    update umirc

--- .umirc.js
+++ .umirc.js
@@ -4,6 +4,7 @@ export default {
   treeShaking: true,
   theme:'./src/thems.js',
   outputPath: './docs',
+  base: '/show',
   plugins: [
     // ref: https://umijs.org/plugin/umi-plugin-react.html
     ['umi-plugin-react', {
